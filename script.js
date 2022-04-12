afdc4f87e943c41ca9647da1b61ed140dbeca743
Lozano Longoria <alozanolon@FVFZMYMEJ1WK.tld>
2022-04-12 08:38:51 -0500

    changed face cards to numbers

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function run() {
 }
 
 function getCard() {
-    var c = Math.floor(Math.random() * 10) + 1
+    var c = Math.floor(Math.random() * 1) + 1
     finalProject.getElementById("card").innerHTML = "<img src=images/2D.jpg>"
 }
