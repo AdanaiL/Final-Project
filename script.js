e389762bb42f3f0c58d43a4180095829cd261ec4
Salazar <fersalazar@FVFZMXVMJ1WK.tld>
2022-04-13 08:27:34 -0500

    first js code

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function run() {
 }
 
 function getCard() {
-    var c = Math.floor(Math.random() * 1) + 1
-    finalProject.getElementById("card").innerHTML = "<img src=images/2D.jpg>"
+    var r = Math.floor(Math.random() * 10) + 1
+    var suitArray = ("C", "D", "H", "S")
+    var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
 }
