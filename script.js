66834f098e53644c4de72168c1d2e1c87fc3457b
Lozano Longoria <alozanolon@FVFZMYMEJ1WK.tld>
2022-04-13 08:39:06 -0500

    Some styling

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function run() {
 }
 
 function getCard() {
-    var r = Math.floor(Math.random() * 10) + 1
+    var r = Math.floor(Math.random() * 13) + 1
     var suitArray = ("C", "D", "H", "S")
-    var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
+    var randomSuit = Math.floor(Math.random() * 4)
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
 }
